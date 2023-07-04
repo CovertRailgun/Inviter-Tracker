038dcec94ec8e6cdd0e6a52fa9186857d4cdc071
CovertRailgun <covertrailgun@gmail.com>
2023-07-03 22:51:54 -0400

    3-7-23

--- cogs/channel_tracker.py
+++ cogs/channel_tracker.py
@@ -1,27 +1,76 @@
-import json
 import pathlib
-import datetime
+import json
 
 import disnake
 from disnake.ext import commands, tasks
 
-
 import settings
 
 
-class test(commands.Cog):
+
+class invite_tracker(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
-        self.location = pathlib.Path(settings.BASE_DIR / 'saved_data' / 'who_invited_n.json')
         self.log = settings.logging.getLogger('bot')
-        self.channel_id = 1062547302885105866
+        self.guild_id = 1122681690683347047
+        self.invites = []
+        self.location = pathlib.Path(settings.BASE_DIR / 'saved_data' / 'who_invited_member.json')
+        self.n = 0
 
-    @commands.slash_command()
-    async def morning(self, inter) -> None:
-        await inter.response.send_message(
-            "Let us know what pokemon you would like see hosted in\nScarlet & Violet by using my /request_pokemon command.\nPokemon with enough votes will be added to the filters in\nRaidcrawler. This command can only be used once a day\nbut you are able to vote for the same Pokemon more than once."
-        )
-        
+    async def cog_load(self) -> None:
+            self.get_invites.start()
+            self.log.info("started invite tracker loop")
+
+    def cog_unload(self) -> None:
+            self.get_invites.stop()
+            self.log.info("stopped invite tracker loop")
+
+    @tasks.loop(seconds=5)
+    async def get_invites(self) -> None:
+        self.n += 1
+        guild = self.bot.get_guild(self.guild_id)
+        self.invites = await guild.invites()
+        self.log.info(f"updated invites {self.n}") 
+
+    @get_invites.before_loop
+    async def before_get_invites(self) -> None:
+        self.log.info("delaying loading get_invite loop")
+        await self.bot.wait_until_ready()
+
+    def find_invite_by_code(self, invite_list:disnake.Guild.invites, code:disnake.Invite.code) -> disnake.invite:
+        self.log.info("finding invite by code")
+        for invite in invite_list:
+            if invite.code == code:
+                return invite
+
+    async def update_join_list(self, member:disnake.member) -> None:
+        self.log.info("Starting update_join_list")
+        with open(self.location, "r+") as file:
+            who_invited = json.load(file)
+            if member.id in who_invited:
+                who_invited[member.id] += 1
+            else:
+                who_invited[member.id] = 1
+            file.seek(0)
+            json.dump(who_invited, file, indent=4) 
+        self.log.info("Finished update_join_list")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member) -> None:
+        self.log.info("member joined")
+        invites_after_join = await member.guild.invites()
+
+        for invite in self.invites:
+            if invite.uses < self.find_invite_by_code(invites_after_join, invite.code).uses:
+                await self.update_join_list(invite.inviter)
+                self.invites = invites_after_join
+                self.log.info("finished adding join to inviters count")
+                return None
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member:disnake.member):
+        self.log.info("member left")
+        self.invites = await member.guild.invites()
 
 def setup(bot: commands.Bot):
-    bot.add_cog(test(bot))
+    bot.add_cog(invite_tracker(bot))
--- cogs/setup_stuff.py
+++ cogs/setup_stuff.py
@@ -8,33 +8,34 @@ class Bot_Setup(commands.Cog):
         self.bot = bot
         self.log = settings.logging.getLogger('bot')
 
-    @commands.slash_command()
+    @commands.slash_command(description="Owner command")
     @commands.is_owner()
     async def z_cog(self, inter:disnake.ApplicationCommandInteraction) -> None:
         pass
 
-    @z_cog.sub_command()
+    @z_cog.sub_command(description="Owner command")
     async def reload(self, inter:disnake.ApplicationCommandInteraction, cog:str) -> None:
         self.log.info(f"reloading cogs.{cog.lower()}")
         self.bot.reload_extension(f"cogs.{cog.lower()}")
         await inter.response.send_message("reloaded!", ephemeral=True)
         self.log.info(f'done reloading {cog.lower()}')
 
-    @z_cog.sub_command()
+    @z_cog.sub_command(description="Owner command")
     async def load(self, inter:disnake.ApplicationCommandInteraction, cog:str) -> None:
         self.log.info(f"loading cogs.{cog.lower()}")
         self.bot.load_extension(f"cogs.{cog.lower()}")
         await inter.response.send_message("loaded!", ephemeral=True)
         self.log.info(f'done loading {cog.lower()}')
 
-    @z_cog.sub_command()
+    @z_cog.sub_command(description="Owner command")
     async def unload(self, inter:disnake.ApplicationCommandInteraction, cog:str) -> None:
         self.log.info(f"unloading cogs.{cog.lower()}")
         self.bot.unload_extension(f"cogs.{cog.lower()}")
         await inter.response.send_message("unloaded!", ephemeral=True)
         self.log.info(f'done unloading {cog.lower()}')
-    
-    async def cog_slash_command_error(self, inter:disnake.ApplicationCommandInteraction, error):
+
+    @z_cog.error    
+    async def z_cog_error_handler(self, inter:disnake.ApplicationCommandInteraction, error):
         self.log.info(f"error raised: {error}")
         if isinstance(error, commands.errors.NotOwner):
             await inter.response.send_message(f'Permission denied {error}', ephemeral=True)
